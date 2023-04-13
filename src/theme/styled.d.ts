d99a6d2577c26ef79fcd187f4afdf7098bbd59c7
Zachary Dorcinville <zdorcinville@gmail.com>
2023-04-13 05:07:25 -0400

    rm comments

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,9 +1,6 @@
 import {} from 'styled-components';
 
-// importing the theme we created
 import theme from '../theme';
-
-// global typescript declaration for the theme
 
 declare module 'styled-components' {
     type Theme = typeof theme;
